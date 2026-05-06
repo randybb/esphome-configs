e4c8c72c2004e43a37af256318427934ab73f7eb
Roman Priesol <roman@priesol.net>
2026-05-07 00:31:06 +0200

    Custom scripts and related device lists are no longer needed

--- update_prod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ESPHOME_IS_HA_ADDON=true
-
-while IFS=, read -r device
-do
-    esphome upload ${device}.yaml --device ${device}.local
-#done < devices_nspanel.csv
-done < devices_prod.csv
